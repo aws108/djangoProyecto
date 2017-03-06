ad16fa075840c67460dcea585dee754c5dd8f723
aws108 <sergiolizangarcia@gmail.com>
2017-03-06 20:24:34 +0100

    django

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,34 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from ecologic import views
+
+urlpatterns = [
+    url(r'^ecologic/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+"""
+urlpatterns = [
+	url(r'^$', views.ItemListView.as_view() ),
+	url(r'^torn$', views.ItemCreateView.as_view(success_url="/list") ),
+	url(r'^list$', views.ItemListView.as_view() ),
+	url(r'^vota$', views.VotaView.as_view() ),
+	url(r'^vota2/(?P<votacio>[0-9]{0,10})/', views.votaActionView ), # view feta amb "metode classic"
+    url(r'^admin/', admin.site.urls ),     # view del sistema d'administracio automatic de Django
+]
+"""
